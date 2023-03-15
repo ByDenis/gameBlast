1a715f4957b8f08a7e47f2a432ea62d5f033adef
byDenis <dstetskiy@live.com>
2023-03-15 18:50:57 +0400

    ui update init

--- src/ui.ts
+++ src/ui.ts
@@ -2,21 +2,17 @@ import gameConfig from "./gameConfig";
 import State from "./state/State";
 
 export default () => {
+    
+    const bombCount:HTMLElement|null = document.getElementById('bomb-count')
+    const pointCount:HTMLElement|null = document.getElementById('point-count')
+    const stepsCount:HTMLElement|null = document.getElementById('steps-count')
+    const bombBtn:HTMLElement|null = document.getElementById('game-bomb')
+
     const state = State.getInstance();
     state.subscribe( (props:any) => {
-        
-        let bombCount:HTMLElement|null = document.getElementById('bomb-count')
         if ( bombCount !== null ) bombCount.innerHTML = props.bombCount
-
-        let pointCount:HTMLElement|null = document.getElementById('point-count')
         if ( pointCount !== null ) pointCount.innerHTML = props.point+' / '+gameConfig.pointsToWin
-
-        let stepsCount:HTMLElement|null = document.getElementById('steps-count')
         if ( stepsCount !== null ) stepsCount.innerHTML = props.steps
-
-
-        let bombBtn:HTMLElement|null = document.getElementById('game-bomb')
-        if ( bombBtn !== null ) props.isBomb === true ? bombBtn.classList.add("on") : bombBtn.classList.remove("on");
-
+        if ( bombBtn !== null ) props.isBomb === true ? bombBtn.classList.add("on") : bombBtn.classList.remove("on")
     });
 }
