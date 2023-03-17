df56c9b5b7fe4c7128b9086a051a22ed4faa149d
byDenis <dstetskiy@live.com>
2023-03-17 13:33:52 +0400

    update bomb rule

--- src/gameConfig.ts
+++ src/gameConfig.ts
@@ -10,10 +10,10 @@ export default {
     colomns: 9, // Колон в игре
     arrGameBlockType: arrGameBlockType, // Типов в игре
     groupLengthToWin: 2, // Сжигаются не менее
-    addBombLength: 4, // Когда добавляется бомба
+    addBombLength: 5, // Когда добавляется бомба
     bombStartValue: 1, // Начальное количество бомб
     bombRadius: 2, // Сила бомбы
     stepsToWin: 20, // Доступно ходов
-    pointsToWin: 100, // Нужно набрать очков
+    pointsToWin: 120, // Нужно набрать очков
     shuffleCount: 5  // Перемешиваний до проигрыша
 }
